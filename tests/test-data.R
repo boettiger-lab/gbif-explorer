f45d8fb00f0966e27a8e664810ff7dfc0e1cdfc2
Carl Boettiger <cboettig@berkeley.edu>
2025-08-26 15:49:21 -0700

    weird behavior on hexes of honolunu?

--- tests/test-data.R
+++ tests/test-data.R
@@ -10,21 +10,21 @@ source("app/data-layers.R")
 source("app/tools.R")
 duckdb_secrets()
 
-"e905ed8e-1eee-425a-8c97-39bd27287abf"
 
 ex <- activate_from_config(
   "0da365a8-6973-41be-a156-2345a7d0f706", # honolulu
   layer_config$county_layer
 )
-ex_hexed <- get_h3_aoi(ex, 7)
+ex_hexed <- get_h3_aoi(ex, 7) # plenty of hits
 
 ex_gdf <- get_richness(
+  # WEIRD!  only one h7 hex finds a match
   poly = ex,
   zoom = as.integer(7),
   taxa_selections = list()
-)
+) # Also weird -- only 50 h9 hexes find a match
 
-
+# but lots of h7 hexes match from state polygon.
 lazy_gdf <- activate_from_config(
   "e905ed8e-1eee-425a-8c97-39bd27287abf", # states later
   layer_config$region_layer
